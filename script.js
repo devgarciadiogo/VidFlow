0858b2073d494b21108d6b2a8c98bdd0d674dcd5
Diogo Garcia <124640510+devgarciadiogo@users.noreply.github.com>
2024-08-02 18:19:09 -0300

    v1.3
    - Utilizando o Async / Await para uso de promisses
    - Usando try/catch para tratamento de erros
    - Criando if e utilizando o throw new error para criar erros personalizados e evitar erros na aplicação

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 const containerVideos = document.querySelector(".videos__container");
 
-const api = fetch("http://localhost:3000/videos") //usando o fetch fiz uma busca do meu videos.json e gerou uma promisse
-.then(res => res.json()) //usei o then para solicitar uma resposta da promisse
-.then((videos) =>
-    videos.forEach((video) => {
-        containerVideos.innerHTML += `
-        <li class ="videos__item">
-            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-            <div class="descricao-video">
-                <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
-                <h3 class"titulo-video">${video.titulo}</h3>
-                <p class="titulo-canal">${video.descricao}</p>
-            </div>
-        </li>
-        `;
-    })
-)
-.catch((error) =>{
-   containerVideos.innerHTML = `<p>Houve um erro ao carregar os videos: ${error}</p>`
-})
+
+async function buscarEMostrarVideos(){
+    try{
+        const busca = await fetch("http://localhost:3000/videos");
+        const videos = await busca.json(); 
+        
+            videos.forEach((video) => {
+                if(video.categoria == ""){
+                    throw new Error('Video não tem categoria')
+                }
+                containerVideos.innerHTML += `
+                <li class ="videos__item">
+                    <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                    <div class="descricao-video">
+                        <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
+                        <h3 class"titulo-video">${video.titulo}</h3>
+                        <p class="titulo-canal">${video.descricao}</p>
+                    </div>
+                </li>
+                `;
+            })
+    } catch(error) {
+        containerVideos.innerHTML = `<p>Houve um erro ao carregar os videos: ${error}</p>`
+    }
+}
+buscarEMostrarVideos();
